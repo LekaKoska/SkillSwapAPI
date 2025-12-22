b71a1eb9f88cb9a9afc9bc0ccaa720978d79a868
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-22 13:49:42 +0100

    * Adding role into users * Creating const of table name

---
+++ database/migrations/2025_12_22_124553_add_role_to_users.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table(User::TABLE, function (Blueprint $table) {
+            $table->string(column: 'role')->default(value: 'user');
+        });
+    }
+    public function down(): void
+    {
+        Schema::table(User::TABLE, function (Blueprint $table) {
+            $table->dropIfExists();
+        });
+    }
+};
