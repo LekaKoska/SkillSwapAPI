a60844b3b275685b469cb18a318af39659768caa
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-22 13:42:06 +0100

    * Creating Auth controller with 3 methods: Register, Login and Logout * Each method except logout have Custom requests * Adding endpoints in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
